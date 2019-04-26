2e9aa0fb7ec4a8555ca496332c2b06cfb426c9f3
g17948tr <g17948tr@gm.tsuda.ac.jp>
2019-04-26 16:32:20 +0900

    Aboutウィンドウを変更

--- src/com/toy/anagrams/ui/About.java
+++ src/com/toy/anagrams/ui/About.java
@@ -80,7 +80,7 @@ public class About extends JDialog {
         copyrightTextArea.setEditable(false);
         copyrightTextArea.setLineWrap(true);
         copyrightTextArea.setRows(8);
-        copyrightTextArea.setText("Anagrams\n\nCopyright (c) 2003  Irritable Enterprises, Inc. \n\nmaskgirls\nChiaki,Yuki,Risa");
+        copyrightTextArea.setText("Anagrams\n\nCopyright (c) 2003  Irritable Enterprises, Inc. \n\nmaskgirls\nC,Y,R");
         copyrightTextArea.setWrapStyleWord(true);
         copyrightTextArea.setBorder(null);
         copyrightTextArea.setFocusable(false);
